2e3c4ad41c2c5b001504394998b6e189b9e00555
allenjoy123 <allen.bandela@gmail.com>
2024-04-16 17:57:44 +0000

    test

--- _01_02b/Employee.java
+++ _01_02b/Employee.java
@@ -4,6 +4,7 @@ public class Employee {
 
   public static void main(String[] args) {
 
+    // test
     // Create a variable called age of type int and assign it the value 29.
 
     // Print the age variable to the console.
